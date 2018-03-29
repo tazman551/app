02c799ac9862ba2b2780d76189ab00b8daaefec7
tazman551 <36387437+tazman551@users.noreply.github.com>
2018-03-29 13:25:13 -0400

    Update db_create.sql

--- db_create.sql
+++ db_create.sql
@@ -24,3 +24,20 @@ foreign key(core_id) REFERENCES core_schedule(core_id)
 );
 
 
+create table week_schedule(
+week_schedule_id int Auto_increment not null unique primary key,
+date_in_use date not null,
+monday VARCHAR(48),
+tuesday VARCHAR(48),
+wednesday VARCHAR(48),
+thrusday VARCHAR(48),
+friday VARCHAR(48)
+);
+
+create table temp_schedule(
+temp_schedule_id int Auto_increment not null unique primary key,
+user_id int,
+core_id int,
+foreign key(user_id) REFERENCES user(user_id)
+foreign key(core_id) REFERENCES core_schedule(core_id)
+);
