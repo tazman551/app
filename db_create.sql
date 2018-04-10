09beaf53a7a001aabd7705a54f5874f786356cc6
tazman551 <36387437+tazman551@users.noreply.github.com>
2018-04-09 20:18:18 -0400

    Update db_create.sql

--- db_create.sql
+++ db_create.sql
@@ -78,10 +78,71 @@ create table tech();
 
 create table operation();
 
+_______________________________NEW __________________________________________
+
+drop database if exists tpp_schedule;
+
+create database tpp_schedule;
+
+use tpp_schedule;
 
 
-//uml in yuml
+create table main_schedule(
+main_id int Auto_increment not null unique primary key,
+date_modified date not null,
+monday VARCHAR(48),
+tuesday VARCHAR(48),
+wednesday VARCHAR(48),
+thrusday VARCHAR(48),
+friday VARCHAR(48)
+);
 
-[User|user_id:int(pk);first_name:VARCHAR(50);last_name:VARCHAR(50);pathway:char(1);core_id:int(fk);]++1-1>[core_schedule|core_id:int(pk);date_modified:date;monday:VARCHAR(48);tuesday:VARCHAR(48);wednesday:VARCHAR(48);thrusday:VARCHAR(48);friday:VARCHAR(48);],[User]++1--++0..*[week_schedule|week_schedule_id:int(pk);date_in_use:date;monday:VARCHAR(48);tuesday:VARCHAR(48);wednesday:VARCHAR(48);thrusday:VARCHAR(48);friday:VARCHAR(48);],[User]++1--++0..*[temp_schedule|
-temp_schedule_id:int(pk);user_id:int;core_id:int;]
+
+create table participants(
+user_id int Auto_increment not null unique primary key,
+first_name VARCHAR(50) not null,
+last_name VARCHAR(50) not null,
+pathway char(1),
+main_id int,
+foreign key(main_id) REFERENCES main_schedule(main_id)
+);
+
+
+create table week_schedule(
+week_schedule_id int Auto_increment not null unique primary key,
+date_in_use date not null,
+monday VARCHAR(48),
+tuesday VARCHAR(48),
+wednesday VARCHAR(48),
+thrusday VARCHAR(48),
+friday VARCHAR(48)
+);
+
+create table temp_schedule(
+temp_schedule_id int Auto_increment not null unique primary key,
+user_id int,
+main_id int,
+foreign key(user_id) REFERENCES participants(user_id),
+foreign key(main_id) REFERENCES main_schedule(main_id)
+);
+
+create table commitment(
+commitment int Auto_increment not null unique primary key,
+title text,
+start_time time,
+end_time time,
+credits int,
+user_id int,
+foreign key(user_id) REFERENCES participants(user_id)
+);
+
+DELIMITER $$
+CREATE TRIGGER `default_date` BEFORE INSERT ON main_schedule FOR EACH ROW
+if ( isnull(new.date_modified) ) then
+ set new.date_modified=curdate();
+end if;
+$$
+delimiter ;
+
+
 
